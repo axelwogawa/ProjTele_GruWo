32c7f1c4188389afdf9b07a59da44a04fa5c66e2
axelwogawa <Axel.Wogawa@mailbox.tu-dresden.de>
2018-01-17 15:01:06 +0100

    Cleaned MainActivity.java a bit; Updated HowToGit.adoc

--- app/app/src/main/java/de/gruwo/handschrifterkennung/MainActivity.java
+++ app/app/src/main/java/de/gruwo/handschrifterkennung/MainActivity.java
@@ -2,22 +2,16 @@ package de.gruwo.handschrifterkennung;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-<<<<<<< 3f00c118891e8be83ee0eeea68d85ea730cd7d03
 import android.graphics.Color;
-=======
-import android.content.res.Resources;
->>>>>>> Made HWR candidates available;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
